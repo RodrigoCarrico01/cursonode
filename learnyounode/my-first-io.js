1ba0baa53baae13472c945d685575080154747c7
Rodrigo Martins <mr.rodrigocarrico@gmail.com>
2025-02-06 10:36:09 +0000

    test 2

--- learnyounode/my-first-io.js
+++
@@ -1,9 +0,0 @@
-
-const fs = require('fs');
-
-const filePath = process.argv[2];
-const fileContent = fs.readFileSync(filePath, 'utf8');
-
-const newLineCount = fileContent.split('\n').length - 1;
-
-console.log(newLineCount);
